793a76d8fc24bf8b3f23b6e777f71d46ed50ac6c
LoveHeart <113758022+LeeyooMuzic@users.noreply.github.com>
2024-05-23 16:21:14 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,11 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/HarshalPurohitEdits/TheMovieProviderBot.git /TheMovieProviderBot 
+  git https://github.com/leeyoomuzic/deluxmovienew/tree/MJ
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /TheMoviesProviderBot
+  git clone $UPSTREAM_REPO /deluxmovienew 
 fi
-cd /THALAPATHY-FILTER-BOT 
+cd /deluxmovienew 
 pip3 install -U -r requirements.txt
-echo "Starting TheMovieProviderBot...."
-python3 bot.py
+echo "Starting Bot...."
