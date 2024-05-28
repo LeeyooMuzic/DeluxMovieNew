41b215705e923d1ba01208e73d1cc86757bf5717
LoveHeart <113758022+LeeyooMuzic@users.noreply.github.com>
2024-05-28 09:41:34 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -21,7 +21,7 @@ logger = logging.getLogger(__name__)
 
 TIMEZONE = "Asia/Kolkata"
 BATCH_FILES = {}
-
+MJ_GL = "https://t.me/+uUs4A3DApnAyMmFl"
 @Client.on_message(filters.command("start") & filters.incoming)
 async def start(client, message):
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
